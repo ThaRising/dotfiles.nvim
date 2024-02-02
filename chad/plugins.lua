1758464cb54fa8b928ebb2d1a55e6c2abc1b483a
koch <koch@skygate.de>
2024-02-02 22:34:32 +0100

    Feature * Add support for Terraform

--- chad/plugins.lua
+++ chad/plugins.lua
@@ -13,6 +13,7 @@ local plugins = {
     end,
     config = function(_, opts)
       dofile(vim.g.base46_cache .. "syntax")
+      opts.ensure_installed = { "lua", "vimdoc", "terraform", "python" }
       require("nvim-treesitter.configs").setup(opts)
     end,
     dependencies = {
--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,9 +5,19 @@ local capabilities = config.capabilities
 
 local lspconfig = require("lspconfig")
 
+-- Python
 lspconfig.pyright.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = {"python"},
 })
 
+-- Terraform
+-- Warning: This requires 'terraform-ls' to be installed on your system
+lspconfig.terraformls.setup{}
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
