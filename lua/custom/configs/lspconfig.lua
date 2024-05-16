67b464ac9b8ae0d76ac70d971b9ea8a833d51845
ben.koch <ben.koch@rapid-data.de>
2024-05-16 10:02:22 +0200

    Fix * Add 'custom' subdirectory

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,37 @@
+local config = require("plugins.configs.lspconfig")
+
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require("lspconfig")
+
+-- Python
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
+
+-- Ansible
+lspconfig.ansiblels.setup({
+  filetypes = { "yaml", "yml", "ansible" },
+  root_dir = lspconfig.util.root_pattern("roles", "playbooks", "ansible", "tasks"),
+})
+
+-- Terraform
+-- Warning: This requires 'terraform-ls' to be installed on your system
+lspconfig.terraformls.setup{}
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
+
+vim.api.nvim_exec([[
+  augroup terraformvars
+    autocmd!
+    autocmd BufRead,BufNewFile *.tfvars set filetype=terraform
+  augroup END
+]], false)
+
---
+++ lua/custom/vscode/plugins.lua
@@ -0,0 +1,15 @@
+local plugins = {
+    {
+        "NvChad/base46",
+        enable = false,
+    },
+    {
+        "NvChad/ui",
+        enable = false
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        enable = false
+    }
+}
+return plugins
