fd435dd10f982649f88eb10d17aebb3f79b4c5dc
ben.koch <ben.koch@rapid-data.de>
2024-06-07 17:14:15 +0200

    Feature * Add command history search

--- lua/custom/chad/plugins.lua
+++ lua/custom/chad/plugins.lua
@@ -1,5 +1,15 @@
 local plugins = {
   {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    config = function()
+      -- calling `setup` is optional for customization
+      require("fzf-lua").setup({})
+    end
+  },
+  {
     "gioele/vim-autoswap",
     lazy = false
   },
